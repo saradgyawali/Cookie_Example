3a46ad5c4bc7466dd461694b95ac2ae9122f5f96
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-01 12:22:28 -0700

    second commit

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ var cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 app.get("/", function(req, res) {
-	res.send("Hello World");
+	res.send("hello world");
 
 });
+
+app.get("/moar_cookie", function(req, res) {
+	res.cookie("wdi", 21);
+	res.send("Check your Cookies");
+
+})
+
+app.get("/sad_cookie", function(req, res) {
+	var val = req.cookies.wdi;
+	console.log(val, typeof(val));
+
+	res.clearCookie("wdi", 21);
+	res.send("Error! No Cookies, Its all GONE!!");
+})
+
+
+
+
 
 app.listen(3000, function(req, res) {
 	console.log("Server is Up and Running")
