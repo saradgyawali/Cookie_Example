0a9b13bd5a85e8d2ef3187ca955442aa0c4ecc62
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-01 12:32:54 -0700

    third commit

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ app.get("/", function(req, res) {
 
 });
 
+// Set a cookie in the HTTP Resonse Header with a key of "wdi" and value of 21 whenever you hit /moar_cookie. See res.cookie.
+// Next, hit your endpoint. From inside your route, can you read the incoming cookies and console.log them?
+// How would you get the value associated with wdi?
+// What datatype is it? Can you prove it?
+
 app.get("/moar_cookie", function(req, res) {
 	res.cookie("wdi", 21);
 	res.send("Check your Cookies");
 
 })
 
+// Remove the "wdi" cookie from the HTTP Response Header whenever you hit /sad_cookie. See res.clearCookie
 app.get("/sad_cookie", function(req, res) {
 	var val = req.cookies.wdi;
 	console.log(val, typeof(val));
@@ -23,6 +29,12 @@ app.get("/sad_cookie", function(req, res) {
 	res.send("Error! No Cookies, Its all GONE!!");
 })
 
+// Let's see if we can count the number of times someone has come to the site, using a cookie. First, set a cookie. Then, update the cookie each time you visit "/", incrementing the number by 1.
+app.get("/", function(req, res) {
+	var count = req.cookies.count;
+	res.cookie("count", count+1);
+	res.send(count+1)
+})
 
 
 
